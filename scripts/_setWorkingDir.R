514570c029dc7a505d908ffc39624ad163f3bca0
Ben Baumer <ben.baumer@gmail.com>
2018-06-22 08:54:50 -0400

    added README.Rmd

--- scripts/_setWorkingDir.R
+++
@@ -1,5 +0,0 @@
-###################################################
-# setting the working directory for all the scripts
-###################################################
-
-setwd("H:/documenti/GitHub/baseball_R/data") # modify the "H:/documenti" part to reflect your local GitHub directory
