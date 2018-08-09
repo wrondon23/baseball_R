ae9f508a8337ee1739a81fb4aa378f010450e315
Ben Baumer <ben.baumer@gmail.com>
2018-08-09 12:11:25 -0400

    cleanup from previous commit

--- scripts/one.simulation.68.R
+++ scripts/one.simulation.68.R
@@ -1,75 +1,79 @@
 one.simulation.68 <- function(s.talent = 0.20){
-
+  require(dplyr)
   make.schedule <- function(teams, k){
     n.teams <- length(teams)
-    Home <- rep(gl(n.teams, n.teams, length=n.teams ^ 2, labels=teams), k)
-    Visitor <- rep(gl(n.teams, 1, length=n.teams ^ 2, labels=teams), k)
-    schedule <- data.frame(Home = Home, Visitor = Visitor)
-    subset(schedule, Home != Visitor)
+    Home <- rep(rep(teams, each=n.teams), k)
+    Visitor <- rep(rep(teams, n.teams), k)
+    schedule <- tibble(Home = Home,
+                           Visitor = Visitor)
+    dplyr::filter(schedule, Home != Visitor)
   }
-  
-  NL <- c("ATL", "CHN", "CIN", "HOU", "LAN", "NYN", "PHI", "PIT", "SFN", "SLN")
-  AL <- c("BAL", "BOS", "CAL", "CHA", "CLE", "DET", "MIN", "NYA", "OAK", "WS2")
+
+  NL <- c("ATL", "CHN", "CIN", "HOU", "LAN",
+          "NYN", "PHI", "PIT", "SFN", "SLN")
+  AL <- c("BAL", "BOS", "CAL", "CHA", "CLE",
+          "DET", "MIN", "NYA", "OAK", "WS2")
   teams <- c(NL, AL)
   league <- c(rep(1, 10), rep(2, 10))
-  
-  schedule <- rbind(make.schedule(NL, 9), 
+
+  schedule <- bind_rows(make.schedule(NL, 9),
                    make.schedule(AL, 9))
 
-  # simulate talents 
-  talents <- rnorm(20, 0, s.talent)  
-  TAL <- data.frame(Team = teams, League = league, Talent = talents)
+  # simulate talents
+  talents <- rnorm(20, 0, s.talent)
+  TAL <- tibble(Team = teams, League = league,
+                    Talent = talents)
 
   # merge talents and win probs with schedule data frame
-  SCH <- merge(schedule, TAL, by.x = "Home", by.y = "Team")
-  names(SCH)[4] <- "Talent.Home"
-  SCH <- merge(SCH, TAL, by.x = "Visitor", by.y = "Team")
-  names(SCH)[6] <- "Talent.Visitor"
-  SCH$prob.Home <- with(SCH,
-          exp(Talent.Home) / (exp(Talent.Home) + exp(Talent.Visitor)))
+  SCH <- schedule %>%
+    inner_join(TAL, by = c("Home" = "Team")) %>%
+    rename(Talent.Home = Talent) %>%
+    inner_join(TAL, by = c("Visitor" = "Team", "League")) %>%
+    rename(Talent.Visitor = Talent)
 
   # play season of games
-  SCH$outcome <- with(SCH, rbinom(nrow(SCH), 1, prob.Home)) 
-  SCH$winner <- with(SCH, ifelse(outcome, as.character(Home), 
-                   as.character(Visitor)))
+  SCH %>% 
+    mutate(prob.Home = exp(Talent.Home) /
+             (exp(Talent.Home) + exp(Talent.Visitor))) -> SCH
+  
+  SCH %>%
+    mutate(outcome = rbinom(nrow(.), 1, prob.Home),
+           winner = ifelse(outcome, Home, Visitor)) -> SCH
 
   # compute number of games won for all teams
-  wins <- table(SCH$winner)
-  WIN <- data.frame(Team = names(wins), Wins = as.numeric(wins))
+  SCH %>% 
+    group_by(winner) %>%
+    summarize(Wins = n()) %>%
+    inner_join(TAL, by = c("winner" = "Team"))   -> RESULTS
 
-  RESULTS <- merge(TAL, WIN)
-
-  win.league <- function(RR, league){
-    wins <- RR$Wins * (RR$League == league)
-    MAX <- max(wins)  
-    if(sum(wins == MAX) > 1){
-      prob <- exp(RR$Talent) * (wins == MAX)
-      outcome <- c(rmultinom(1, 1, prob))
-      RR$Winner.Lg <- RR$Winner.Lg + outcome
-      }
-  
-    if(sum(wins == MAX) == 1){
-    RR$Winner.Lg <- RR$Winner.Lg + as.numeric(wins == MAX)}
-    RR
+  win_league <- function(RR) {
+    out <- RR %>%
+      mutate(Winner.Lg = 0, 
+             prob = exp(Talent),
+             outcome = sample(nrow(.), prob = prob)) %>%
+      arrange(League, desc(Wins), outcome) %>%
+      select(-outcome)
+    out[1 + c(0, nrow(RR) / 2), "Winner.Lg"] <- 1
+    out
   }
 
 # record if eligible for wild card (Wild), in playoffs (Playoff)
 # in conference playoff (CS), World Series (WS), or winner (B)
 
-  RESULTS$Winner.Lg <- 0; RESULTS$Winner.WS <- 0;  
-
-  # figure out winners of each league
-  for(j in 1:2)
-    RESULTS <- win.league(RESULTS, j)
-
-# play World Series
-
-  teams <- (1:20)[RESULTS$Winner.Lg == 1]
-  outcome <- c(rmultinom(1, 7, exp(RESULTS$Talent)[teams]))
-  winner <- teams[1] * (diff(outcome) < 0) + teams[2] * (diff(outcome) > 0)
-  RESULTS$Winner.WS[winner] <- 1
-
+  RESULTS <- win_league(RESULTS)
+  
+  ws_winner <- RESULTS %>%
+    filter(Winner.Lg == 1) %>%
+    mutate(outcome = rmultinom(1, 7, prob),
+           Winner.WS = ifelse(outcome > 3, 1, 0)) %>%
+    filter(outcome > 3) %>%
+    select(winner, Winner.WS)
+  
   # data frame has teams, division, talent, wins, and diff playoff results
-  RESULTS
+  RESULTS %>%
+    left_join(ws_winner, by = c("winner")) %>%
+    replace_na(list(Winner.WS = 0)) %>% 
+    rename(Team = winner) %>% 
+    select(- prob)
 }
 
